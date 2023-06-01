4d5afc13e97814fa13ef6cb9f322a29dd367408b
kevin <kmathew@peloton-technologies.com>
2023-05-31 22:37:54 -0700

    fixing code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {useContext} from 'react';
+import { useContext } from 'react';
 import HomePageLayout from './components/templates/HomePageLayout.js';
 import BlogPageLayout from './components/templates/BlogPageLayout.js';
 import UserRegistrationPageLayout from './components/templates/UserRegistrationPageLayout.js'
 import SimplePageLayout from './components/templates/SimplePageLayout.js'
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Platform from './components/pages/Platform.js';
 import Login from './components/pages/Login.js';
 import NewsAndEvents from './components/pages/NewsAndEvents.js';
@@ -13,29 +13,31 @@ import Resources from './components/pages/Resources.js';
 import AdminPanel from './components/pages/AdminPanel.js';
 import Leadership from './components/pages/Leadership.js';
 import UserRegistration from './components/pages/UserRegistration.js';
-import {UserProvider} from './auth/UserProvider.js';
-import {UserContext} from './auth/UserProvider.js';
+import { UserProvider } from './auth/UserProvider.js';
+import { UserContext } from './auth/UserProvider.js';
 
 import UserAccount from './components/pages/UserAccount.js';
 
 function App() {
-  
 
-  const {user, setUserInfo, logout} = useContext(UserContext);
+
+  const { user, setUserInfo, logout } = useContext(UserContext);
 
   return (
-   <UserProvider value ={user, setUserInfo, logout}> 
+    <UserProvider value={user, setUserInfo, logout}>
+      <BrowserRouter>
         <Switch>
-        	<Route path="/" component={HomePageLayout} exact />
-        	<Route path="/contactus" component={UserRegistration} exact />
-        	<Route path="/leadership" component={Leadership} exact />
-        	<Route path="/news" component={NewsAndEvents} />
-        	<Route path="/platform" component={Platform} />
-        	<Route path="/login" component={Login} />
+          <Route path="/" component={HomePageLayout} exact />
+          <Route path="/contactus" component={UserRegistration} exact />
+          <Route path="/leadership" component={Leadership} exact />
+          <Route path="/news" component={NewsAndEvents} />
+          <Route path="/platform" component={Platform} />
+          <Route path="/login" component={Login} />
           <Route path="/account" component={UserAccount} />
           <Route path="/resources" component={Resources} />
           <Route path="/adminpanel" component={AdminPanel} />
         </Switch>
+      </BrowserRouter>
     </UserProvider>
   );
 }
